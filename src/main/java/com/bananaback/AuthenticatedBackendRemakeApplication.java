8d6974de36c9cd5f9da570060c9d5bd76e5d542e
bananaback <58079308+bananaback@users.noreply.github.com>
2023-08-16 15:43:50 +0700

    Initial commit

---
+++ src/main/java/com/bananaback/AuthenticatedBackendRemakeApplication.java
@@ -0,0 +1,13 @@
+package com.bananaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatedBackendRemakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatedBackendRemakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bananaback/AuthenticatedBackendRemakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bananaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatedBackendRemakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
